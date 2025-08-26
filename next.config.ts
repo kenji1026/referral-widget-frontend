0d1d685127c95ebb369c5a1237a48839e2cfb6d6
cheeming <dragon.steal75@gmail.com>
2025-08-27 08:40:40 +0900

    feat: add next project

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: false,
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+};
+
+export default nextConfig;
