477a4a5b9599af51e3846ae939984dfb8f127238
cheeming <dragon.steal75@gmail.com>
2025-10-03 01:49:15 +0900

    fix: add image remote module

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.celeste-paris.fr",
+        port: "",
+        pathname: "/cdn/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
   reactStrictMode: false,
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
